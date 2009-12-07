529b037ec510c54504f5c0507cba3eaf9df3bf7e
non88sense <non88sense@e43db140-ba3d-11de-8be4-e1425cda3908>
2009-12-07 18:20:05 +0000

    added second test case

--- test/com/ontologycentral/ldquery/QueryEngineTest.java
+++ test/com/ontologycentral/ldquery/QueryEngineTest.java
@@ -9,7 +9,20 @@ import com.ontologycentral.ldspider.hooks.error.ErrorHandler;
 import com.ontologycentral.ldspider.hooks.error.ErrorHandlerLogger;
 
 public class QueryEngineTest extends TestCase {
-	public void testQuery() {
+	public void testQuery1() {
+		String query = "SELECT * WHERE { <http://harth.org/andreas/foaf#ah> <http://xmlns.com/foaf/0.1/knows> ?o . }" ;
+
+		QueryEngine qe = new QueryEngine();
+
+		ErrorHandler eh = new ErrorHandlerLogger();
+		qe.setErrorHandler(eh);
+		
+		qe.setOutputCallback(new CallbackNQOutputStream(System.out));
+
+		qe.evaluate(query);
+	}
+
+	public void testQuery2() {
 		String query = "SELECT * WHERE { <http://harth.org/andreas/foaf#ah> ?p ?o . ?o ?p2 ?o2 . }" ;
 
 		QueryEngine qe = new QueryEngine();
