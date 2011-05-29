080622ecaeeabe610c6beb7603b50994e764cd15
non88sense <non88sense@e43db140-ba3d-11de-8be4-e1425cda3908>
2011-05-29 18:52:08 +0000

    reverted to callback parser

--- src/com/ontologycentral/ldspider/hooks/content/ContentHandlerRdfXml.java
+++ src/com/ontologycentral/ldspider/hooks/content/ContentHandlerRdfXml.java
@@ -6,8 +6,7 @@ import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.semanticweb.yars.nx.Node;
-import org.semanticweb.yars.nx.Nodes;
+import org.semanticweb.yars.nx.Resource;
 import org.semanticweb.yars.nx.parser.Callback;
 import org.semanticweb.yars.nx.parser.ParseException;
 import org.semanticweb.yars2.rdfxml.RDFXMLParser;
@@ -35,15 +34,15 @@ public class ContentHandlerRdfXml implements ContentHandler {
 
 	public boolean handle(URI uri, String mime, InputStream source, Callback callback) {
 		try {
-//			RDFXMLParser r = new RDFXMLParser(source, true, false, uri.toString(), callback, new Resource(uri.toString()));
-			RDFXMLParser r = new RDFXMLParser(source, true, false, uri.toString());
-			while (r.hasNext()) {
-				Node[] nx = r.next();
-				
-				callback.processStatement(nx);
-				
-				//_log.info("processing " + Nodes.toN3(nx));
-			}
+			RDFXMLParser r = new RDFXMLParser(source, true, true, uri.toString(), callback, new Resource(uri.toString()));
+//			RDFXMLParser r = new RDFXMLParser(source, true, false, uri.toString());
+//			while (r.hasNext()) {
+//				Node[] nx = r.next();
+//				
+//				callback.processStatement(nx);
+//				
+//				//_log.info("processing " + Nodes.toN3(nx));
+//			}
 			//, callback, new Resource(uri.toString()))
 			
 			return true;
